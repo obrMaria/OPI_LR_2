ccb425d471aed7de4e0676699f375cb6e943eeb0
obrMaria <midsummer.madness@mail.ru>
2022-09-14 15:25:03 +0300

    commit

---
+++ C++ project/C++ project/C++ project.cpp
@@ -0,0 +1,13 @@
+﻿#include<Windows.h>
+#include<iostream>
+#include<stdio.h>
+#include<cmath>
+#include<conio.h>
+
+using namespace std;
+int main()
+{
+	cout << ("first commit") << endl;
+
+	return 0;
+}
