d9d3c84f6f53f4c0067039170ccf262b470de600
obrMaria <midsummer.madness@mail.ru>
2022-09-14 15:37:08 +0300

    our program is successful created

--- C++ project/C++ project/C++ project.cpp
+++ C++ project/C++ project/C++ project.cpp
@@ -15,7 +15,30 @@ int main()
 	cout << ("введите t: ");
 	cin >> t;
 
-	cout << ("t=")<< t << endl;
+	
+	if (t <= 1)
+	{
+		f = a * pow(t, 2) - b * sqrt(t + 1);
+		cout << f << endl;
+	}
+	else if(1 < t && t <= 2)
+	{
+		f = a - b;
+		cout << f << endl;
+	}
+	else if(t == 2.3)
+	{
+		f = a * pow(t, (2 / 3)) - pow(b, 3) * sqrt(t + 1);
+		cout << f << endl;
+	}
+	else if(t > 2.3 && t < 3)
+	{
+		f = pow(t, (a * b));
+		cout << f << endl;
+	}
+	else
+		cout << ("нет решения") << endl;
+
 
 
 	return 0;
